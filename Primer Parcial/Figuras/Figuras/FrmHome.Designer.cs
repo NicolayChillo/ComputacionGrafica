e15c3eab6c53a547becd8e32d8e2483b6739fb53
NicolayChillo <nicolaychillo13@gmail.com>
2025-05-07 22:52:07 -0500

    Figuras

---
+++ Primer Parcial/Figuras/Figuras/FrmHome.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Figuras
+{
+    partial class FrmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.regularesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.irregularesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.curasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cuadradoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estrellaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rectánguloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.triánguloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trapecioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trapezoideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.romboToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.romboideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deltoideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.círculoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.semicírculoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.elipseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.regularesToolStripMenuItem,
+            this.irregularesToolStripMenuItem,
+            this.curasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(782, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // regularesToolStripMenuItem
+            // 
+            this.regularesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cuadradoToolStripMenuItem,
+            this.estrellaToolStripMenuItem});
+            this.regularesToolStripMenuItem.Name = "regularesToolStripMenuItem";
+            this.regularesToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
+            this.regularesToolStripMenuItem.Text = "Regulares";
+            // 
+            // irregularesToolStripMenuItem
+            // 
+            this.irregularesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rectánguloToolStripMenuItem,
+            this.triánguloToolStripMenuItem,
+            this.trapecioToolStripMenuItem,
+            this.trapezoideToolStripMenuItem,
+            this.romboToolStripMenuItem,
+            this.romboideToolStripMenuItem,
+            this.deltoideToolStripMenuItem});
+            this.irregularesToolStripMenuItem.Name = "irregularesToolStripMenuItem";
+            this.irregularesToolStripMenuItem.Size = new System.Drawing.Size(93, 24);
+            this.irregularesToolStripMenuItem.Text = "Irregulares";
+            // 
+            // curasToolStripMenuItem
+            // 
+            this.curasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.círculoToolStripMenuItem,
+            this.semicírculoToolStripMenuItem,
+            this.elipseToolStripMenuItem});
+            this.curasToolStripMenuItem.Name = "curasToolStripMenuItem";
+            this.curasToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.curasToolStripMenuItem.Text = "Curas";
+            // 
+            // cuadradoToolStripMenuItem
+            // 
+            this.cuadradoToolStripMenuItem.Name = "cuadradoToolStripMenuItem";
+            this.cuadradoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.cuadradoToolStripMenuItem.Text = "Cuadrado";
+            // 
+            // estrellaToolStripMenuItem
+            // 
+            this.estrellaToolStripMenuItem.Name = "estrellaToolStripMenuItem";
+            this.estrellaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.estrellaToolStripMenuItem.Text = "Estrella";
+            // 
+            // rectánguloToolStripMenuItem
+            // 
+            this.rectánguloToolStripMenuItem.Name = "rectánguloToolStripMenuItem";
+            this.rectánguloToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.rectánguloToolStripMenuItem.Text = "Rectángulo";
+            // 
+            // triánguloToolStripMenuItem
+            // 
+            this.triánguloToolStripMenuItem.Name = "triánguloToolStripMenuItem";
+            this.triánguloToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.triánguloToolStripMenuItem.Text = "Triángulo";
+            // 
+            // trapecioToolStripMenuItem
+            // 
+            this.trapecioToolStripMenuItem.Name = "trapecioToolStripMenuItem";
+            this.trapecioToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.trapecioToolStripMenuItem.Text = "Trapecio";
+            // 
+            // trapezoideToolStripMenuItem
+            // 
+            this.trapezoideToolStripMenuItem.Name = "trapezoideToolStripMenuItem";
+            this.trapezoideToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.trapezoideToolStripMenuItem.Text = "Trapezoide";
+            // 
+            // romboToolStripMenuItem
+            // 
+            this.romboToolStripMenuItem.Name = "romboToolStripMenuItem";
+            this.romboToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.romboToolStripMenuItem.Text = "Rombo";
+            // 
+            // romboideToolStripMenuItem
+            // 
+            this.romboideToolStripMenuItem.Name = "romboideToolStripMenuItem";
+            this.romboideToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.romboideToolStripMenuItem.Text = "Romboide";
+            // 
+            // deltoideToolStripMenuItem
+            // 
+            this.deltoideToolStripMenuItem.Name = "deltoideToolStripMenuItem";
+            this.deltoideToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.deltoideToolStripMenuItem.Text = "Deltoide";
+            // 
+            // círculoToolStripMenuItem
+            // 
+            this.círculoToolStripMenuItem.Name = "círculoToolStripMenuItem";
+            this.círculoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.círculoToolStripMenuItem.Text = "Círculo";
+            // 
+            // semicírculoToolStripMenuItem
+            // 
+            this.semicírculoToolStripMenuItem.Name = "semicírculoToolStripMenuItem";
+            this.semicírculoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.semicírculoToolStripMenuItem.Text = "Semicírculo";
+            // 
+            // elipseToolStripMenuItem
+            // 
+            this.elipseToolStripMenuItem.Name = "elipseToolStripMenuItem";
+            this.elipseToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.elipseToolStripMenuItem.Text = "Elipse";
+            // 
+            // FrmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmHome";
+            this.Text = "FrmHome";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem regularesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cuadradoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem estrellaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem irregularesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem curasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rectánguloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem triánguloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trapecioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trapezoideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem romboToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem romboideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deltoideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem círculoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem semicírculoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem elipseToolStripMenuItem;
+    }
+}
